4c7ec124f0c53c0dc901d0c61389bce1d12ce7ec
Jean-LucG <72151965+Jean-LucG@users.noreply.github.com>
2020-12-07 15:21:00 +0100

    Update Destination.js

--- JS/Destination.js
+++ JS/Destination.js
@@ -1,13 +1,38 @@
+
+
+
+
 class Destination {
-    constructor (id, lieu, petit_dej, prix_adulte, nb_adultes, nb_enfants) {
-        this.id = id
+    constructor (id, prix, image, lieu ) {
+        this.id = id;
+        this.prix = prix;
+        this.image = image;
         this.lieu = lieu;
-        this.petit_dej = petit_dej;
-        this.prix_adulte = prix_adulte;
-        this.nb_adultes = nb_adultes;
-        this.nb_enfants = nb_enfants;
-    }
-    prix_total () {
-        return this.prix_adulte * (this.nb_adultes + 0,4) + 12 * petit_dej;
     }
 }
+
+let moscou = new destination(0, 300, '/images/Moscou.jpg', 'Moscou');
+let bamako = new destination(1, 617, '/images/Ajaccio.jpg', 'Ajjaccio');
+let ajaccio = new destination(2, 457, '/images/Ajaccio.jpg', 'Ajaccio');
+let milan = new destination(3, 358, '/images/Milan.jpg', 'Madrid');
+let paris= new destination(4, 404, '/images/Paris.jpg', 'Paris');
+let abidjan = new destination(5, 856, '/images/Abidjan.jpg', 'Abidjan');
+let mexico= new destination(6, 770,'/images/Mexico.jpg', 'Mexico');
+let riodejaneiro= new destination(7, 666, '/images/RiodeJaneiro.jpg', 'Rio de Janeiro');
+let beyrouth = new destination(8, 412, '/images/Beyrouth.jpg', 'Beyrouth');
+let bagdad = new destination(9, 400, '/images/Bagdad.jpg', 'Bagdad');
+let lyon = new destination(10, 13, '/images/Lyon.jpg', 'Lyon');
+let tokyo = new destination(11, 805, '/images/Tokyo.jpg', 'Tokyo');
+let pekin = new destination(12, 707, '/images/Pekin.jpg', 'Pekin');
+let shangai = new destination(13, 212, '/images/Shangai.jpg', 'Shangai');
+let stalingrad= new destination(14, 798, '/images/Stalingrad.jpg', 'Stalingrad');
+let tchernobyl = new destination(15, 1000, '/images/Tchernobyl.jpg', 'Tchernobyl');
+let auschwitz= new destination(16, 444, '/images/Auschwitz.jpg', 'Auschwitz');
+let athenes = new destination(17, 130, '/images/athenes.jpg', 'Athènes');
+let hanoi = new destination(18, 520, '/images/Hanoi.jpg', 'Hanoï');
+let guatemala = new destination(19, 945, '/images/Guatemala.jpg', 'Guatemala');
+let kiev = new destination(20, 477, '/images/Kiev.jpg', 'Kiev');
+let zagreb = new destination(21, 169, '/images/Zagreb.jpg', 'Zagreb');
+let kampala = new destination(22, 148, '/images/Kampala.jpg', 'Kampala');
+let pyongyang= new destination(23, 510, '/images/Pyongyang.jpg', 'Pyongyang');
+let lalune = new destination(24, 100000, '/images/LaLune.jpg', 'New York');
